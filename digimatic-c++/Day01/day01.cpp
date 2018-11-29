2b0e267fbec86c62adbbc6bb45b041a72a3bf7c2
Peter Westerstrom <peter@westerstrom.com>
2018-11-29 15:25:54 +0100

    Preparing for AoC 2018

---
+++ digimatic-c++/Day01/day01.cpp
@@ -0,0 +1,27 @@
+// Advent of Code 2018 - Day 1
+// Peter Westerström (digimatic)
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace std::string_literals;
+
+auto input = ""s;
+
+void solve_day1_part1()
+{
+	cout << "Day 1 - part 1: " << endl;
+}
+
+void solve_day1_part2()
+{
+	cout << "Day 1 - part 2: "<< endl;
+}
+
+int main()
+{
+	solve_day1_part1();
+	solve_day1_part2();
+	return 0;
+}
