5eac081ca999b220ba246a8d357a3cc1fd040cd1
Peter Westerström <peter@westerstrom.com>
2018-12-01 06:26:14 +0100

    Day 1 solutions in C++

--- digimatic-c++/Day01/day01.cpp
+++ digimatic-c++/Day01/day01.cpp
@@ -1,22 +1,71 @@
 // Advent of Code 2018 - Day 1
 // Peter Westerström (digimatic)
 
+#include "config.h"
+
+#include <common/common.h>
 #include <iostream>
 #include <string>
+#include <unordered_set>
+#include <stdexcept>
 
+using namespace westerstrom;
 using namespace std;
 using namespace std::string_literals;
 
-auto input = ""s;
+
+int parseLine(const string& line)
+{
+	auto remaining = line;
+	size_t taken = 0;
+	auto n = std::stoi(remaining, &taken);
+	return n;
+}
 
 void solve_day1_part1()
 {
-	cout << "Day 1 - part 1: " << endl;
+	auto lines = readLines(INPUT_FILE);
+	int currentFreq = 0;
+	for(auto& line : lines)
+	{
+		auto freqChange = parseLine(line);
+		currentFreq += freqChange;
+	}
+
+	
+	cout << "Day 1 - part 1: " << currentFreq << endl;
+}
+
+
+int findFirstDuplicate()
+{
+	auto lines = readLines(INPUT_FILE);
+	int currentFreq = 0;
+	unordered_set<int> seenFrequences;
+	while(true)
+	{
+		for(auto& line : lines)
+		{
+			auto freqChange = parseLine(line);
+			currentFreq += freqChange;
+			if(seenFrequences.find(currentFreq) != seenFrequences.end())
+				return currentFreq;
+			seenFrequences.insert(currentFreq);
+		}
+	}
+	throw std::exception();
 }
 
 void solve_day1_part2()
 {
-	cout << "Day 1 - part 2: "<< endl;
+	try
+	{
+		auto f = findFirstDuplicate();
+		cout << "Day 1 - part 2: "<< f << endl;
+	} catch(const std::exception&)
+	{
+		cout << "Day 1 - part 2: no solution found."<<  endl;
+	}
 }
 
 int main()
