9a4639a83a7da2084890ae35ca48465339826112
neochrome <johan@stenqvist.net>
2018-12-01 22:50:02 +0100

    neochrome-ocaml day 01

---
+++ neochrome-ocaml/ext.ml
@@ -0,0 +1,24 @@
+module File = struct
+
+  let open_in filename fn =
+    let ch = open_in filename in
+    try
+      let res = fn ch in
+      close_in ch;
+      res
+    with e ->
+      close_in ch;
+      raise e
+  ;;
+
+end
+
+module Seq = struct
+  include Seq
+
+  let of_lines ch =
+    let rec next () =
+      try Cons (input_line ch, next)
+      with End_of_file -> Nil
+    in next
+end
