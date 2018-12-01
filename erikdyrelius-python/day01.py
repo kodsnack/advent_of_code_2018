d8be838311ea3cd0e47900f049de04de36c76bba
Erik Dyrelius <erik.dyrelius@gmail.com>
2018-12-01 06:23:04 +0100

    Non-fancy solution to day 1.

--- erikdyrelius-python/day01.py
+++ erikdyrelius-python/day01.py
@@ -5,6 +5,18 @@ inp = readInput()
 def parseTokens(inp, f):
     for line in inp.strip().splitlines():
         for token in line.split():
-            f(token)
+            yield f(token)
 
-parseTokens(inp, print)
+def iterUntilRepeat(inp):
+    s = set()
+    sm = 0
+    while True:
+        for i in parseTokens(inp, int):
+            sm += i
+            if sm in s:
+                return sm
+            else:
+                s.add(sm)
+
+print("Solution to day 1 part 1:", sum(parseTokens(inp, int)))
+print("Solution to day 1 part 2:", iterUntilRepeat(inp))
