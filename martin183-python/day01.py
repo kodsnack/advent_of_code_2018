9304f835dc463a7ee4592b3ac545b8a89a6e725e
Martin Nordgren <martin@Martins-MacBook.lan>
2018-12-01 08:12:39 +0100

    Day 1 added.

---
+++ martin183-python/day01.py
@@ -0,0 +1,39 @@
+#/usr/bin/python
+# AoC2018 day1 A
+############################################################
+import numpy as np
+cur_state = 0
+
+with open('data/day1') as f:
+  for line in f:
+
+    c = line[1:-1]
+    if line[0] == '+':
+      cur_state += int(c)
+    if line[0] =='-':
+      cur_state-= int(c)
+print('Last state is: ' + str(cur_state))
+
+# AoC2018 day1 B
+############################################################
+import numpy as np
+
+cur_state = 0
+states = set()
+found = False
+
+while not(found):
+  with open('data/day1') as f:
+    for line in f:
+
+      c = line[1:-1]
+      if line[0] == '+':
+        cur_state += int(c)
+      if line[0] =='-':
+        cur_state-= int(c)
+
+      if cur_state in states:
+        print(cur_state)
+        found = True
+        break
+      states.add('First duplicated state: ' + str(cur_state))
