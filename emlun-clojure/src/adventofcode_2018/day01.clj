f00f96cf69be584cd28002e750e7aa781d73ee27
Emil Lundberg <emil@emlun.se>
2018-12-01 21:02:18 +0100

    Simplify first-recurrence

--- emlun-clojure/src/adventofcode_2018/day01.clj
+++ emlun-clojure/src/adventofcode_2018/day01.clj
@@ -4,25 +4,14 @@
   (map read-string lines)
 )
 
-(defn first-recurrence
-  { :test #(assert (= 5 (first-recurrence [1 5 2 5 2]))) }
-
-  ( [future]
-    { :pre (seq? future) }
-      (first-recurrence #{} future)
-  )
-
-  ( [history [present & future]]
-    { :pre [(or (seq? future) (nil? future)) (coll? history)] }
-      (if (nil? present)
-        nil
-        (if (contains? history present)
-          present
-          (recur (conj history present) future)
-        )
-      )
-  )
-)
+(defn first-recurrence [coll]
+  (loop [history #{}
+         [present & future] coll
+         ]
+    (if-not (nil? present)
+      (if (contains? history present)
+        present
+        (recur (conj history present) future)))))
 
 (defn run
   [input-lines & args]
