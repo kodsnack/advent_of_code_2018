1c8418a7afd88a2a14bc4ec25b12e73bd0922ab0
Stonemannen <lucas.drufva@live.se>
2018-12-02 06:39:23 +0100

    day2part2

--- stonemannen-javascript/day2.js
+++ stonemannen-javascript/day2.js
@@ -21,4 +21,35 @@ for (var i = 0; i < input.length; i++) {
 }
 console.log("part1: " + doubles * triples);
 
+function findStringDiff(str1, str2) {
+    var compareString = function(str1, str2) {
+        var a1 = str1.split("");
+        var a2 = str2.split("");
+        var idx2 = 0;
+        a1.forEach(function(val) {
+            if (a2[idx2] === val) {
+              a2.splice(idx2,1);
+            } else {
+                idx2 += 1;
+            }
+        });
+        if (idx2 > 0) {
+            a2.splice(idx2,a2.length);
+        }
+        return a2.join("");
+    }
 
+    if (str1.length < str2.length) {
+        return compareString(str1, str2);
+    } else {
+        return compareString(str2, str1);
+    }
+}
+
+for(var i = 0; i < input.length; i++){
+    for(var j = i+1; j < input.length; j++){
+        if(findStringDiff(input[i],input[j]).length == 1){
+            console.log("part2: " + input[i].replace(findStringDiff(input[i],input[j]),""))
+        }
+    }
+}
