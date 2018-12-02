d68479debe50978fcbcb6fe7c316dcd61c31ef0e
Fluff <dune@fluffigt.net>
2018-12-02 12:22:28 +0100

    Rework day 1, removing perl choosing the java path

--- fluff-java/01/a.java
+++ fluff-java/01/a.java
@@ -4,15 +4,13 @@ import java.util.*;
 class Day01A {
 
     public static void main(String[] args) {
+		int freq = 0;
 
-	int freq = 0;
-	
-	Scanner in = new Scanner(System.in);
-	while(in.hasNext()) {
-	    int change = Integer.valueOf(in.nextLine());
-	    freq += change;
-	}
-	in.close();
-	System.out.println(freq);
+		Scanner in = new Scanner(System.in);
+		while(in.hasNext())
+			freq += Integer.parseInt(in.nextLine());
+
+		in.close();
+		System.out.println(freq);
     }
 }
