411d73f27827fc5dff3093e0df07de80ddd828d9
Emil Lundberg <emil@emlun.se>
2018-12-04 21:12:43 +0100

    Simplify parsing

--- emlun-clojure/src/adventofcode/2018/day04.clj
+++ emlun-clojure/src/adventofcode/2018/day04.clj
@@ -1,8 +1,8 @@
 (ns adventofcode.2018.day04)
 
-(def guard-pattern #"\[(\d{4}-\d{2}-\d{2}) (\d{2}:\d{2})\] Guard \#(\d+) begins shift")
-(def sleep-pattern #"\[(\d{4}-\d{2}-\d{2}) (\d{2}):(\d{2})\] falls asleep")
-(def wake-pattern  #"\[(\d{4}-\d{2}-\d{2}) (\d{2}):(\d{2})\] wakes up")
+(def guard-pattern #"\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2})\] Guard \#(\d+) begins shift")
+(def sleep-pattern #"\[(\d{4}-\d{2}-\d{2} (\d{2}):(\d{2}))\] falls asleep")
+(def wake-pattern  #"\[(\d{4}-\d{2}-\d{2} (\d{2}):(\d{2}))\] wakes up")
 
 (defn read-num [s] (read-string (if (= \0 (first s)) (apply str (rest s)) s)))
 
@@ -11,18 +11,16 @@
         sleep-matches (re-matches sleep-pattern line)
         wake-matches (re-matches wake-pattern line)
         ]
-    (if-let [[_ date time id] guard-matches]
-      {:type :guard, :date date, :time time, :id id}
+    (if-let [[_ tstamp id] guard-matches]
+      {:type :guard, :timestamp tstamp, :id id}
 
-      (if-let [[_ date h m] sleep-matches]
-        {:type :sleep, :date date, :time (str h ":" m), :hour (read-num h), :minute (read-num m)}
+      (if-let [[_ tstamp h m] sleep-matches]
+        {:type :sleep, :timestamp tstamp, :hour (read-num h), :minute (read-num m)}
 
-        (if-let [[_ date h m] wake-matches]
-          {:type :wake, :date date, :time (str h ":" m), :hour (read-num h), :minute (read-num m)}
+        (if-let [[_ tstamp h m] wake-matches]
+          {:type :wake, :timestamp tstamp, :hour (read-num h), :minute (read-num m)}
         )
     ))))
-
-(defn timestamp [event] (str (:date event) " " (:time event)))
 
 (defn add-sleep [guard-map start-event end-event]
   (reduce
@@ -59,7 +57,7 @@
 
 (defn run [input-lines & args]
   (let [events (map parse-event input-lines)
-        sorted-events (sort-by timestamp events)
+        sorted-events (sort-by :timestamp events)
         guard-maps (all-sleeps sorted-events)
         ]
     { :A (solve guard-maps strategy-a)
