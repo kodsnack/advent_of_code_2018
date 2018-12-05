3c3c543290a8437f2f95d8f6ff8c6484cf1c3902
Erik Dyrelius <erik.dyrelius@scania.com>
2018-12-05 15:31:04 +0100

    More optimal and nicer looking day 5 solution

--- erikdyrelius-python/day05.py
+++ erikdyrelius-python/day05.py
@@ -2,34 +2,33 @@ from aocbase import readInput
 import re
 
 inp = readInput()
+#inp = "dabAcCaCBAcCcaDA"
 def estrip(inp):
     change = True
-    s = inp
+    s = list(inp)
     while change:
         change = False
         i = 0
         n = []
-        while i < len(s)-1:
-            c1, c2 = s[i], s[i+1]
-            if (('a'<=c1<='z' and ord(c1)-32 == ord(c2)) or
-                ('A'<=c1<='Z' and ord(c1)+32 == ord(c2))):
-                i = i + 1
+        skip = False
+        for c1, c2 in zip(s, s[1:]+[' ']):
+            if skip == True:
+                skip = False
+                continue
+            if c1 != c2 and c1.lower() == c2.lower():
+                skip = True
                 change = True
             else:
                 n.append(c1)
-            i = i + 1
-        if i == len(s)-1:
-            n.append(s[-1])
-        s = ''.join(n)
+        s = n
     return s
-s4 = estrip(inp)
-print("Solution to day 5 part 1:", len(s4))
-s = inp
-s2 = set(s4.lower())
-m = len(s)
-for i in s2:
-    s3 = ''.join(map(lambda x,y=i:'' if x==y or ord(x)+32 == ord(y) else x, s4))
-    es = estrip(s3)
-    m = min(m, len(es))
-    print(i, len(es))
-print("Solution to day 5 part 2:", m)
+resPolymer = estrip(inp)
+print("Solution to day 5 part 1: {}".format(len(resPolymer)))
+
+unitTypes = set(map(lambda x:x.lower(), resPolymer))
+minUnitStrLen = len(inp)
+for unitType in unitTypes:
+    impPolymer = ''.join(map(lambda x,ut=unitType:'' if x.lower()==ut else x, resPolymer))
+    impPolymerTry = estrip(impPolymer)
+    minUnitStrLen = min(minUnitStrLen, len(impPolymerTry))
+print("Solution to day 5 part 2:", minUnitStrLen)
