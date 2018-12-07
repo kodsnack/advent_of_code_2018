2854b4ce9d3d17fc4ba143686ce31f19bfc574ae
Johan Sundell <johansundell@gmail.com>
2018-12-07 19:05:16 +0000

    Cleanup

--- johansundell-go/day7/main.go
+++ johansundell-go/day7/main.go
@@ -15,19 +15,14 @@ type instructionType struct {
 	str string
 }
 
+type worker struct {
+}
+
 func (i *instructionType) removeS(s string) string {
 	return strings.Replace(i.str, s, "", -1)
 }
 
 type instructionsType []instructionType
-
-/*
-func (i instructionsType) removeS(s string) (instructionsType) {
-	in :=
-	for _, r := range i {
-		r.removeS(s)
-	}
-}*/
 
 func main() {
 	data, err := adventofcode2017.GetInput("day7.txt")
@@ -37,25 +32,20 @@ func main() {
 	fmt.Println(parseData(data))
 }
 
-func parseData(input string) string {
+func parseData(input string) (part1 string) {
 	data := strings.Split(input, "\n")
 	list := make(listType)
-	//	keys := ""
 	for _, r := range data {
 		str := strings.Fields(r)
-		//fmt.Println(str[1], str[7])
 		list[str[7]] += str[1]
 		list[str[1]] += ""
 	}
 
-	//fmt.Println(list)
 	instructions := make(instructionsType, 0)
 	for k, r := range list {
-		i := instructionType{key: k, str: r}
-		instructions = append(instructions, i)
+		instructions = append(instructions, instructionType{key: k, str: r})
 	}
-	//fmt.Println(instructions)
-	res := ""
+
 	for i := 0; len(instructions) > 0; i++ {
 		sort.Slice(instructions, func(i, j int) bool {
 			if len(instructions[i].str) == len(instructions[j].str) {
@@ -64,12 +54,11 @@ func parseData(input string) string {
 			return len(instructions[i].str) < len(instructions[j].str)
 		})
 		key := instructions[0].key
-		res += key
+		part1 += key
 		for n, r := range instructions {
 			instructions[n].str = r.removeS(key)
 		}
 		instructions = append(instructions[:0], instructions[1:]...)
-		//fmt.Println(instructions, res)
 	}
-	return res
+	return
 }
