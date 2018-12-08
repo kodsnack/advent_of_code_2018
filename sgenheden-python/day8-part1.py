d405806146ce2ad64af6fe1044f48fa2c6684f91
Samuel Genheden <samuel.genheden@gmail.com>
2018-12-08 11:46:35 +0100

    Dag 8

---
+++ sgenheden-python/day8-part1.py
@@ -0,0 +1,15 @@
+
+
+from day8_utils import read_tree
+
+
+def sum_meta(node):
+    summa = sum(node['meta'])
+    for child in node['children']:
+        summa += sum_meta(child)
+    return summa
+
+
+if __name__ == "__main__":
+    tree = read_tree()
+    print(f"Sum of meta = {sum_meta(tree)}")
