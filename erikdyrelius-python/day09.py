076863a8b9b9257590698bd76432e22337e6d7e6
Erik Dyrelius <erik.dyrelius@gmail.com>
2018-12-09 21:59:35 +0100

    Nice solution to day 09.

--- erikdyrelius-python/day09.py
+++ erikdyrelius-python/day09.py
@@ -1,70 +1,21 @@
 from aocbase import readInput
-import re
-import array
-#493 players; last marble is worth 71863 points
+from collections import deque
 
-def insert(circ, sz, p, v):
-    for i in range(sz, p, -1):
-        circ[i] = circ[i-1]
-    circ[p] = v
-
-def delete(circ, sz, p):
-    for i in range(p, sz-1):
-        circ[i] = circ[i+1]
-
-class marble:
-    def __init__(self, val, prv, nxt):
-        self.v, self.p, self.n = val, prv, nxt
-        if val==0:
-            self.n = self
-            self.p = self
-        else:
-            self.n.p = self
-            self.p.n = self
-    def remove(self):
-        self.p.n = self.n
-        self.n.p = self.p
-    def counter(self, n):
-        while n>0:
-            self = self.p
-            n -= 1
-        return self
-    def clock(self, n):
-        while n>0:
-            self = self.n
-            n -= 1
-        return self
-    def print(self):
-        i = self
-        while True:
-            print(i.v, end=' ')
-            i = i.n
-            if i==self:
-                break
-        print()
-
-def marbles(marb, elves):
-    cur = first = marble(0, None, None)
-    m, e = 1, 0
+def marbles(nofMarbels, nofElves):
+    board = deque()
+    board.append(0)
+    theMarble, theElf = 1, 1
     score = dict()
-    while m < marb:
-        if m % 23 == 0:
-            score[e+1] = score.get(e+1, 0) + m
-            p = cur.counter(7)
-            score[e+1] = score[e+1] + p.v
-            cur = p.n
-            p.remove()
+    while theMarble < nofMarbels:
+        if theMarble % 23 == 0:
+            board.rotate(7)
+            score[theElf] = score.get(theElf, 0) + theMarble + board.popleft()
         else:
-            p1 = cur.clock(1)
-            p2 = p1.clock(1)
-            cur = marble(m, p1, p2)
-        #print(e+1, end=":")
-        #first.print()
-        e = (e + 1)%elves
-        m += 1
-    return score
+            board.rotate(-2)
+            board.appendleft(theMarble)
+        theElf = (theElf % nofElves) + 1
+        theMarble += 1
+    return max(score.values())
 
-i = marbles(71863, 493)
-print("Solution to day 9 part 1:", max(i.values()))
-i = marbles(71863*100, 493)
-print("Solution to day 9 part 2:", max(i.values()))
+print("Solution to day 9 part 1:", marbles(71863, 493))
+print("Solution to day 9 part 2:", marbles(71863*100, 493))
