d190b473d899e28578fc388e95e4106dc6a6548d
Fredrik Broman <fredrik.broman@me.com>
2018-12-10 21:34:46 +0100

    Day 1 Exercise 1

---
+++ fredrikb-rust/README.txt
@@ -0,0 +1,4 @@
+https://adventofcode.com/2018
+
+cd dayX
+cargo run --release < path/to/input.txt
---
+++ fredrikb-rust/day1/src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+use std::io::Read;
+use std::process;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read stdin.");
+
+    let mut result = 0;
+
+    for line in input.lines() {
+        let change: i32 = line.parse().expect("Couldn't cast line to i32");
+        result += change;
+    }
+    println!("Result = {}", result);
+    process::exit(0)
+}
