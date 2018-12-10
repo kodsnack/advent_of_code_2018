dd63378db08cdc606c19646d4aba12e59d6e79ca
Nerarith <cawe00@gmail.com>
2018-12-10 23:45:00 +0100

    Day 9 and 10 in c++

---
+++ nerarith-c++/day9/9b.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#if DEBUG
+#include "prettyprint.hpp"
+#define PRINTLN(x) \
+    cerr << x << "\n"
+#define PRINTSP(x) \
+    cerr << x << " "
+#else
+#define PRINTLN(x)
+#define PRINTSP(x)
+#endif
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+template<class T> void amax(T & a, const T & b) { a = max(a,b); }
+template<class T> void amin(T & a, const T & b) { a = min(a,b); }
+
+template<class T> T getinword () {
+    T temp;
+    cin >> temp;
+    return temp;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long nplayers, nmarbles;
+    scanf("%lld players; last marble is worth %lld points", & nplayers, & nmarbles);
+    nmarbles *= 100;
+
+    vector<long long> score (nplayers, 0);
+    vector<long long> marbles = {0};
+    long long curr=-1;
+    for (long long i=1; i <= nmarbles; i++) {
+        if (i % 23 != 0) {
+            if (curr == marbles.size()-2) {
+                curr = marbles.size();
+            }
+            else {
+                curr = (curr + 2) % marbles.size();
+            }
+            marbles.insert(marbles.begin()+curr, i);
+        }
+        else {
+            curr = (curr - 7 + marbles.size()) % marbles.size();
+            score[(i-1)%nplayers] += i + marbles[curr];
+            marbles.erase(marbles.begin()+curr);
+        }
+        if (i % (nmarbles/100) == 0) {
+            PRINTLN("i/(nmarbles/100):  " << i/(nmarbles/100));
+            if ((i/(nmarbles/100)) % 20 == 0) {
+                PRINTLN("marbles:  " << marbles);
+                PRINTLN("score:  " << score);
+            }
+        }
+    }
+
+    long long mx=0;
+    for (long long i=0; i < nplayers; i++) {
+        amax(mx, score[i]);
+    }
+    cout << mx << "\n";
+}
