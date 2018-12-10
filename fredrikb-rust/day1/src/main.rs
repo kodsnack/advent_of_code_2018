059c332500b67f782cb5b68c67fa2342fdfcfe85
Fredrik Broman <fredrik.broman@me.com>
2018-12-10 21:48:57 +0100

    Cargo fmt

--- fredrikb-rust/day1/src/main.rs
+++ fredrikb-rust/day1/src/main.rs
@@ -1,15 +1,17 @@
-use std::process;
-use std::io::Read;
 use std::io;
+use std::io::Read;
+use std::process;
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input).expect("Failed to read stdin.");
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("Failed to read stdin.");
 
     let mut result = 0;
-    
+
     for line in input.lines() {
-        let change: i32 = line.parse().expect("Couldn't cast line to i32"); 
+        let change: i32 = line.parse().expect("Couldn't cast line to i32");
         result += change;
     }
     println!("Result = {}", result);
