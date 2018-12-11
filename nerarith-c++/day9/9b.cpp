ead6403e25b973e4da67469f1068caa6a1a80640
Nerarith <cawe00@gmail.com>
2018-12-11 16:55:54 +0100

    day 11 and second part of day 9 in c++

--- nerarith-c++/day9/9b.cpp
+++ nerarith-c++/day9/9b.cpp
@@ -24,44 +24,52 @@ template<class T> T getinword () {
     return temp;
 }
 
+template<typename It>
+It step_forward (It beg, It end, It it, int n) {
+    for (int i=0; i < n; i++) {
+        if (it == end)
+            it = beg;
+        it++;
+    }
+    return it;
+}
+
+template<typename It>
+It step_backwards (It beg, It end, It it, int n) {
+    for (int i=0; i < n; i++) {
+        if (it == beg)
+            it = end;
+        it--;
+    }
+    return it;
+}
+
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    long long nplayers, nmarbles;
-    scanf("%lld players; last marble is worth %lld points", & nplayers, & nmarbles);
+    int nplayers, nmarbles;
+    scanf("%d players; last marble is worth %d points", & nplayers, & nmarbles);
     nmarbles *= 100;
 
     vector<long long> score (nplayers, 0);
-    vector<long long> marbles = {0};
-    long long curr=-1;
-    for (long long i=1; i <= nmarbles; i++) {
+    list<int> marbles = {0};
+    list<int>::iterator curr = marbles.begin();
+    for (int i=1; i <= nmarbles; i++) {
         if (i % 23 != 0) {
-            if (curr == marbles.size()-2) {
-                curr = marbles.size();
-            }
-            else {
-                curr = (curr + 2) % marbles.size();
-            }
-            marbles.insert(marbles.begin()+curr, i);
+            curr = step_forward(all(marbles), curr, 2);
+            curr = marbles.insert(curr, i);
         }
         else {
-            curr = (curr - 7 + marbles.size()) % marbles.size();
-            score[(i-1)%nplayers] += i + marbles[curr];
-            marbles.erase(marbles.begin()+curr);
-        }
-        if (i % (nmarbles/100) == 0) {
-            PRINTLN("i/(nmarbles/100):  " << i/(nmarbles/100));
-            if ((i/(nmarbles/100)) % 20 == 0) {
-                PRINTLN("marbles:  " << marbles);
-                PRINTLN("score:  " << score);
-            }
+            curr = step_backwards(all(marbles), curr, 7);
+            score[(i-1)%nplayers] += i + *curr;
+            curr = marbles.erase(curr);
         }
     }
 
     long long mx=0;
-    for (long long i=0; i < nplayers; i++) {
+    for (int i=0; i < nplayers; i++) {
         amax(mx, score[i]);
     }
     cout << mx << "\n";
