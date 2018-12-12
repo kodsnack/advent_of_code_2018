bed331f331f5f626b2d7447e6e8bba939c230a9e
Johan Sundell <johansundell@gmail.com>
2018-12-12 13:27:56 +0100

    Cleanup

--- johansundell-go/day11/main.go
+++ johansundell-go/day11/main.go
@@ -12,14 +12,8 @@ type cell struct {
 
 func (c cell) power(serial int) int {
 	rackId := c.x + 10
-	i := rackId * c.y
-	i += serial
-	//fmt.Println(i)
-	i = i * rackId
-	//fmt.Println(i)
+	i := ((rackId * c.y) + serial) * rackId
 	runes := strings.Split(fmt.Sprintf("%d", i), "")
-	//fmt.Println(runes)
-	//fmt.Println(runes[len(runes)-3])
 	i, _ = strconv.Atoi(string(runes[len(runes)-3]))
 	return i - 5
 }
