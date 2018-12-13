a159b9c7682828de6917a7341fe130ff3def04b0
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2018-12-13 12:49:47 +0100

    Bit nicer 13a

--- estomagordo-python3/day_13a.py
+++ estomagordo-python3/day_13a.py
@@ -1,23 +1,67 @@
-from collections import defaultdict
+def make_cart(x, y, c):
+    cart = [x, y, 0]
+
+    if c == '>':
+        cart.append(0)
+    elif c == 'v':
+        cart.append(1)
+    elif c == '<':
+        cart.append(2)
+    else:
+        cart.append(3)
+
+    return cart
+
+def turn(symbol, direction, mode):
+    if symbol == '/':
+        if direction == 0:
+            direction = 3
+        elif direction == 1:
+            direction = 2
+        elif direction == 2:
+            direction = 1
+        else:
+            direction = 0
+    elif symbol == '\\':
+        if direction == 0:
+            direction = 1
+        elif direction == 1:
+            direction = 0
+        elif direction == 2:
+            direction = 3
+        else:
+            direction = 2
+    elif symbol == '+':
+        if mode == 0:
+            direction = (direction - 1) % 4
+        elif mode == 2:
+            direction = (direction + 1) % 4
+        mode = (mode + 1) % 3
+
+    return direction, mode
+
+
+def move(x, y, direction):
+    if direction == 0:
+        x += 1
+    elif direction == 1:
+        y += 1
+    elif direction == 2:
+        x -= 1
+    else:
+        y -= 1
+
+    return x, y
 
 
 def solve(d):
-    moved = defaultdict(int)
     carts = []
 
     for y, line in enumerate(d):
         for x, c in enumerate(line):
             if c not in '>v<^':
                 continue
-            carts.append([x, y, 0])
-            if c == '>':
-                carts[-1].append(0)
-            elif c == 'v':
-                carts[-1].append(1)
-            elif c == '<':
-                carts[-1].append(2)
-            else:
-                carts[-1].append(3)
+            carts.append(make_cart(x, y, c))
                 
     while True:
         mv = set()
@@ -25,50 +69,18 @@ def solve(d):
         
         for cart_number, cart in enumerate(carts):
             x, y, mode, direction = cart
-            
-            if direction == 0:
-                x += 1
-            elif direction == 1:
-                y += 1
-            elif direction == 2:
-                x -= 1
-            else:
-                y -= 1
-
-            moved[cart_number] += 1
-
+            x, y = move(x, y, direction)
+           
             for cn, cart in enumerate(carts):
                 if cart[:2] == [x, y]:
                     if cn in mv or (cart[3] != direction and not (cart[3] + direction) % 2):
                         return ','.join(map(str, [x, y]))
 
-            if d[y][x] == '/':
-                if direction == 0:
-                    direction = 3
-                elif direction == 1:
-                    direction = 2
-                elif direction == 2:
-                    direction = 1
-                else:
-                    direction = 0
-            elif d[y][x] == '\\':
-                if direction == 0:
-                    direction = 1
-                elif direction == 1:
-                    direction = 0
-                elif direction == 2:
-                    direction = 3
-                else:
-                    direction = 2
-            elif d[y][x] == '+':
-                if mode == 0:
-                    direction = (direction - 1) % 4
-                elif mode == 2:
-                    direction = (direction + 1) % 4
-                mode = (mode + 1) % 3            
+            direction, mode = turn(d[y][x], direction, mode)
             
             carts[cart_number] = [x, y, mode, direction]
             mv.add(cart_number)
+            
 
 def read_and_solve():
     with open('input_13.txt') as f:
