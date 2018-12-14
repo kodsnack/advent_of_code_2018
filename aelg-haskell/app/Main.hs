7baff6c447cfdfe04f5623161a5d3ef78e783a69
Linus Mellberg <linus.mellberg@infinera.com>
2018-12-14 21:12:08 +0100

    Day 14

--- aelg-haskell/app/Main.hs
+++ aelg-haskell/app/Main.hs
@@ -26,6 +26,7 @@ import qualified Day10
 import qualified Day11
 import qualified Day12
 import qualified Day13
+import qualified Day14
 
 solved =
   M.fromList
@@ -42,6 +43,7 @@ solved =
     , (11, Day11.solve)
     , (12, Day12.solve)
     , (13, Day13.solve)
+    , (14, Day14.solve)
     ]
 
 getSolution x = M.findWithDefault notImplemented x solved
