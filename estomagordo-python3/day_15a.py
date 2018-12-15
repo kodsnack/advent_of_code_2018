fc0e55addeffa6c6a5137bdf710c3678d2d3fa95
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2018-12-15 11:11:28 +0100

    work, eh destruction, in progress

--- estomagordo-python3/day_15a.py
+++ estomagordo-python3/day_15a.py
@@ -1,3 +1,5 @@
+from heapq import heappop, heappush
+
 def get_neighbours(x, y, width, height):
     neighbours = []
 
@@ -13,21 +15,100 @@ def get_neighbours(x, y, width, height):
     return neighbours
 
 
-def find_target(d, width, height, x, y, target):
-    targets = {}
+def get_distance(d, width, height, x1, y1, x2, y2):
+    if x1 == x2 and y1 == y2:
+        return 0
+    if not 0 <= x1 < width:
+        return 100000
+    if not 0 <= y1 < height:
+        return 100000
+    if d[y1][x1] != '.':
+        return 100000
 
-    seen = {}
+    seen = set()
+    frontier = [[x1, y1, 0]]
+    pos = 0
+
+    while pos < len(frontier):
+        dx, dy, distance = frontier[pos]
+
+        pos += 1
+
+        if (dx, dy) in seen:
+            continue
+
+        seen.add((dx, dy))
+
+        if dx == x2 and dy == y2:
+            return distance
+
+        for nx, ny in get_neighbours(dx, dy, width, height):
+            if d[ny][nx] == '.' and not (nx, ny) in seen:
+                frontier.append([nx, ny, distance + 1])
+
+    return 100000
+
+
+def find_goal(d, width, height, x, y, target):
+    goals = {}
+    seen = set()
 
     frontier = []
 
     if y > 0 and d[y - 1][x] == '.':
-        frontier.append([x, y - 1, 1])
+        heappush(frontier, [1, x, y - 1])
     if y < height - 1 and d[y + 1][x] == '.':
-        frontier.append([x, y + 1, 1])
+        heappush(frontier, [1, x, y + 1])
     if x > 0 and d[y][x - 1] == '.':
-        frontier.append([x - 1, y, 1])
+        heappush(frontier, [1, x - 1, y])
     if x < width - 1 and d[y][x + 1] == '.':
-        frontier.append([x + 1, y, 1])
+        heappush(frontier, [1, x + 1, y])
+
+    while frontier:
+        distance, dx, dy = heappop(frontier)
+
+        if (dx, dy) in seen:
+            continue
+
+        seen.add((dx, dy))
+
+        neighbours = get_neighbours(dx, dy, width, height)
+
+        for nx, ny in neighbours:
+            if d[ny][nx] == target:
+                goals[(dx, dy)] = distance
+                break
+            if not (nx, ny) in seen and d[ny][nx] == '.':
+                heappush(frontier, [distance + 1, nx, ny])
+
+    if not goals:
+        return None
+
+    lowest = min(goals.values())
+
+    if lowest == 10000:
+        return None
+
+    for sx in range(x + 1, width):
+        if (sx, y) in goals and goals[(sx, y)] == lowest:
+            return sx, y
+    
+    for sy in range(y + 1, height):
+        for sx in range(width):
+            if (sx, sy) in goals and goals[(sx, sy)] == lowest:
+                return sx, sy
+
+    for sy in range(y):
+        for sx in range(width):
+            if (sx, sy) in goals and goals[(sx, sy)] == lowest:
+                return sx, sy
+
+    for sx in range(x):
+        if (sx, y) in goals and goals[(sx, y)] == lowest:
+            return sx, y
+
+    return None
+
 
 def distance_to_target(d, width, height, x, y, target):
     if d[y][x] != '.':
@@ -147,10 +228,16 @@ def solve(d):
                             d[y][x - 1] = '.'
 
                 else:
-                    up_dist = distance_to_target(d, width, height, x, y - 1, target_type)
-                    left_dist = distance_to_target(d, width, height, x - 1, y, target_type)
-                    right_dist = distance_to_target(d, width, height, x + 1, y, target_type)
-                    down_dist = distance_to_target(d, width, height, x, y + 1, target_type)
+                    goal = find_goal(d, width, height, x, y, target_type)
+                    if not goal:
+                        continue
+
+                    gx, gy = goal
+
+                    right_dist = get_distance(d, width, height, x + 1, y, gx, gy)
+                    down_dist = get_distance(d, width, height, x, y + 1, gx, gy)
+                    up_dist = get_distance(d, width, height, x, y - 1, gx, gy)
+                    left_dist = get_distance(d, width, height, x - 1, y, gx, gy)
 
                     closest_dist = min([up_dist, left_dist, right_dist, down_dist])
 
@@ -160,7 +247,21 @@ def solve(d):
                     newx = x
                     newy = y
 
-                    if up_dist == closest_dist:
+                    if right_dist == closest_dist:
+                        hits, _ = elves[(x, y)] if is_elf else goblins[(x, y)]
+                        del moving_dict[(x, y)]
+                        moving_dict[(x + 1, y)] = [hits, True]
+                        d[y][x] = '.'
+                        d[y][x + 1] = c
+                        newx += 1                                        
+                    elif down_dist == closest_dist:
+                        hits, _ = elves[(x, y)] if is_elf else goblins[(x, y)]
+                        del moving_dict[(x, y)]
+                        moving_dict[(x, y + 1)] = [hits, True]
+                        d[y][x] = '.'
+                        d[y + 1][x] = c
+                        newy += 1
+                    elif up_dist == closest_dist:
                         hits, _ = elves[(x, y)] if is_elf else goblins[(x, y)]
                         del moving_dict[(x, y)]
                         moving_dict[(x, y - 1)] = [hits, True]
@@ -174,20 +275,6 @@ def solve(d):
                         d[y][x] = '.'
                         d[y][x  - 1] = c
                         newx -= 1
-                    elif right_dist == closest_dist:
-                        hits, _ = elves[(x, y)] if is_elf else goblins[(x, y)]
-                        del moving_dict[(x, y)]
-                        moving_dict[(x + 1, y)] = [hits, True]
-                        d[y][x] = '.'
-                        d[y][x + 1] = c
-                        newx += 1
-                    else:
-                        hits, _ = elves[(x, y)] if is_elf else goblins[(x, y)]
-                        del moving_dict[(x, y)]
-                        moving_dict[(x, y + 1)] = [hits, True]
-                        d[y][x] = '.'
-                        d[y + 1][x] = c
-                        newy += 1
 
                     possible_targets = get_neighbours(newx, newy, width, height)
                     targets = [p for p in possible_targets if ((p[0], p[1]) in goblins and is_elf) or ((p[0], p[1]) in elves and not is_elf)]
