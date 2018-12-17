e2ae184086715e1d593731a59f5cbc829d430709
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2018-12-17 10:18:56 +0100

    Working 17a

--- estomagordo-python3/day_17a.py
+++ estomagordo-python3/day_17a.py
@@ -1,5 +1,36 @@
 import re
 
+def print_state(clay, resting, reached):
+    min_x = 10000
+    max_x = -10000
+    min_y = 10000
+    max_y = -10000
+
+    for collection in (resting, reached):
+        for x, y in collection:
+            min_x = min(min_x, x)
+            max_x = max(max_x, x)
+            min_y = min(min_y, y)
+            max_y = max(max_y, y)
+
+    with open('17_display.txt', 'w') as f:    
+        for y in range(min_y - 1, max_y + 1):
+            line = []
+            for x in range(min_x - 1, max_x + 1):
+                if x == 500 and y == 0:
+                    line.append('+')
+                elif (x, y) in clay:
+                    line.append('#')
+                elif (x, y) in resting:
+                    line.append('~')
+                elif (x, y) in reached:
+                    line.append('|')
+                else:
+                    line.append('.')
+            f.write(''.join(line))
+            f.write('\n')
+    
+    print(min_x, max_x, min_y, max_y)
 
 def solve(d):
     pattern = re.compile('\d+')
@@ -21,20 +52,21 @@ def solve(d):
             for x in range(numbers[1], numbers[2] + 1):
                 clay.add((x, numbers[0]))
     
+    starts = set([(500, max(1, y_limits[0]))])
+
     def is_free(x, y):
         return (x, y) not in clay and (x, y) not in resting
 
     
     def flow_down(x, y):
         reached.add((x, y))
-        print(len(reached))
-        print(x, y)
         dy = y
+        new_starts = set()
             
         while is_free(x, dy + 1):
             dy += 1
             if dy > y_limits[1]:
-                return
+                return new_starts
             reached.add((x, dy))
         
         left_limit = x
@@ -50,7 +82,7 @@ def solve(d):
                 break
         
         if left_limit == right_limit and dy == y:
-            return
+            return new_starts
 
         for dx in range(left_limit, right_limit + 1):
             reached.add((dx, dy))
@@ -58,21 +90,30 @@ def solve(d):
                 resting.add((dx, dy))
 
         if not is_free(left_limit, dy + 1) and not is_free(right_limit, dy + 1):
-            flow_down(x, y)
-            return
+            new_starts.add((x, y))
+            return new_starts
 
         if is_free(left_limit, dy + 1):
-            flow_down(left_limit, dy + 1)
+            new_starts.add((left_limit, dy))
         if is_free(right_limit, dy + 1):
-            flow_down(right_limit, dy + 1)
+            new_starts.add((right_limit, dy))
 
-    flow_down(500, max(0, y_limits[0]))
-    
+        return new_starts
+
+    for _ in range(2652):
+        number = len(reached)
+        new_starts = set(starts)
+        for x, y in starts:
+            new_starts |= flow_down(x, y)
+        starts = new_starts
+        if len(reached) == number:
+            break
+            
     return len(reached)
         
 
 def read_and_solve():
-    with open('input_17_small.txt') as f:
+    with open('input_17.txt') as f:
         data = [line.rstrip() for line in f]
         return solve(data)
 
