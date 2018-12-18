afe094c0f4369ee9abddf42e435248f754ead0b0
Erik Dyrelius <erik.dyrelius@gmail.com>
2018-12-18 21:45:40 +0100

    Tidy if not faster oslution

--- erikdyrelius-python/day18.py
+++ erikdyrelius-python/day18.py
@@ -14,20 +14,17 @@ inp2 = '''.#.#...|#.
 ...#.|..|.'''
 
 def parse(s):
-    l = []
-    for line in s.splitlines():
-        l.append(list(line))
-    return l
+    return list(s.splitlines())
 
 def gen(l):
     l2 = list()
     szx, szy = len(l[0]), len(l)
     sm = {'|':0, '#':0, '.':0}
     for y in range(szy):
-        l2.append([])
+        l3 = list()
         for x in range(szx):
+            sm['|'] = sm['.'] = sm['#'] = 0
             for dx in range(-1, 2):
-                sm['|'] = sm['.'] = sm['#'] = 0
                 for dy in range(-1, 2):
                     if dx==0 and dy==0:
                         continue
@@ -36,48 +33,54 @@ def gen(l):
                         continue
                     if 0>y2 or y2>=len(l):
                         continue
-                    c = l[y2][x2]
-                    sm[c] += 1
+                    sm[l[y2][x2]] += 1
             c = l[y][x]
             if c=='.':
                 if sm['|']>=3:
-                    l2[y].append('|')
+                    l3.append('|')
                 else:
-                    l2[y].append('.')
+                    l3.append('.')
             elif c=='|':
                 if sm['#']>=3:
-                    l2[y].append('#')
+                    l3.append('#')
                 else:
-                    l2[y].append('|')
+                    l3.append('|')
             elif c=='#':
                 if sm['#']>=1 and sm["|"]>=1:
-                    l2[y].append('#')
+                    l3.append('#')
                 else:
-                    l2[y].append('.')
+                    l3.append('.')
             else:
                 print("Error")
+        l2.append(''.join(l3))
     return l2
 
 def prnt(l):
-    for line in l:
-        print(''.join(line))
+    print('\n'.join(line))
+
+def shortTimeValue(tm, l):
+    for i in range(tm):
+        l = gen(l)
+    s = ''.join(l)
+    return s.count("#")*s.count("|")
+
+def longTimeValue(tm, l):
+    cache = [0]
+    i = 1
+    while True:
+        l = gen(l)
+        s = ''.join(l)
+        rv = s.count("#")*s.count("|")
+        if i==tm:
+            return rv
+        if cache.count(rv) >= 2:
+            first = cache.index(rv)
+            second = cache.index(rv, first+1)
+            if tm%(second-first) == second%(second-first):
+                return rv
+        cache.append(rv)
+        i += 1
 
 l = parse(inp)
-cache = [0]
-for i in range(1, 2000):
-    l = gen(l)
-    #prnt(l)
-    #print()
-    s = ''.join(map(lambda x:''.join(x),l))
-    #print(s)
-    rv = s.count("#")*s.count("|")
-    if cache.count(rv) >= 2:
-        first = cache.index(rv)
-        second = cache.index(rv, first+1)
-        if 1000000000%(second-first) == second%(second-first):
-            print(rv)
-            break
-    cache.append(rv)
-
-print("Solution to day 18 part 1:",18.1)
-print("Solution to day 18 part 2:",18.2)
+print("Solution to day 18 part 1:",shortTimeValue(10, l))
+print("Solution to day 18 part 2:",longTimeValue(1000000000, l))
