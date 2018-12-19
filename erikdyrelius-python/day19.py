8b42f511081e2db984bad1b86ea841489b18fd54
Erik Dyrelius <erik.dyrelius@scania.com>
2018-12-19 12:32:49 +0100

    Complete day 19 solution including analysis of the machine code

--- erikdyrelius-python/day19.py
+++ erikdyrelius-python/day19.py
@@ -4,15 +4,6 @@ from collections import deque
 
 inp = readInput()
 p = re.compile(r"(\w+) (\d+) (\d+) (\d+)")
-inp2 = '''#ip 0
-seti 5 0 1
-seti 6 0 2
-addi 0 1 0
-addr 1 2 3
-setr 1 0 0
-seti 8 0 4
-seti 9 0 5'''
-
 
 def doAddr(inst, reg):
     reg[inst[3]] = reg[inst[1]] + reg[inst[2]]
@@ -80,31 +71,13 @@ def doEqrr(inst, reg):
     else:
         reg[inst[3]] = 0
 
-d = {
-    "addr":doAddr,
-    "addi":doAddi,
-    "mulr":doMulr,
-    "muli":doMuli,
-    "banr":doBanr,
-    "bani":doBani,
-    "borr":doBorr,
-    "bori":doBori,
-    "setr":doSetr,
-    "seti":doSeti,
-    "gtir":doGtir,
-    "gtri":doGtri,
-    "gtrr":doGtrr,
-    "eqir":doEqir,
-    "eqri":doEqri,
-    "eqrr":doEqrr
-}
-
 def parse(s):
     l = []
     for line in s.splitlines():
         m = p.match(line)
         if m:
-            l.append([d[m.group(1)]] + list(map(int, m.groups()[1:])))
+            funcName = "do"+m.group(1)[0].upper()+m.group(1)[1:]
+            l.append([globals()[funcName]] + list(map(int, m.groups()[1:])))
         else:
             if line[0]=='#':
                 ip = int(line[4])
@@ -113,19 +86,30 @@ def parse(s):
     return l, ip
 
 def run(ip, prog, reg=[0]*6):
+    debug =  reg[0]>0
     while True:
         instr = prog[reg[ip]]
-        print(reg, instr)
         instr[0](instr, reg)
         reg[ip] += 1
+        if reg[ip] == 1:
+            return reg[3]
         if reg[ip] < 0 or reg[ip] >= len(prog):
             break
-    return reg[0]
 
+def calcResult(n):
+    i = 1
+    sm = 0
+    while i*i <= n:
+        if n % i == 0:
+            if i*i != n:
+                sm += i
+                sm += n//i
+            else:
+                sm += i
+        i += 1
+    return sm
 
 instr, ip = parse(inp)
-print(run(ip, instr))
-print(run(ip, instr, [1, 0, 0, 0, 0, 0]))
 
-print("Solution to day 19 part 1:",19.1)
-print("Solution to day 19 part 2:",19.2)
+print("Solution to day 19 part 1:",calcResult(run(ip, instr)))
+print("Solution to day 19 part 2:",run(ip, instr, [1, 0, 0, 0, 0, 0]))
