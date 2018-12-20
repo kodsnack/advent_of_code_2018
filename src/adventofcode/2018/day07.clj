9b23fbab777f1fa1e02116e0527805a1c532b0fd
Emil Lundberg <emil@emlun.se>
2018-12-20 23:48:31 +0100

    Solve day 7b

--- src/adventofcode/2018/day07.clj
+++ src/adventofcode/2018/day07.clj
@@ -30,6 +30,25 @@ Step F must be finished before step E can begin.")
                    )
    })
 
+(defn start-timed [dependencies]
+  {:time 0
+   :completed []
+   :remaining (->> dependencies
+                   (vals)
+                   (apply concat)
+                   (reduce (fn [deps task]
+                             (update deps task #(or % #{})))
+                           dependencies
+                           )
+                   )
+   :workers {0 [nil 0]
+             1 [nil 0]
+             2 [nil 0]
+             3 [nil 0]
+             4 [nil 0]
+          }
+   })
+
 (defn step [state]
   (let [next-task (->> state
                        (:remaining)
@@ -50,7 +69,76 @@ Step F must be finished before step E can begin.")
                                   )))
         )))
 
-(defn finished? [state] (empty? (:remaining state)))
+(defn task-time [task]
+  (+ 60 (- (int task) 64)))
+
+(defn assign-work [state]
+  (let [available-tasks (->> state
+                             (:remaining)
+                             (filter (fn [[task prereqs]]
+                                       (empty? prereqs)))
+                             (map first)
+                             (sort)
+                             )
+        available-workers (->> state
+                               (:workers)
+                               (filter (fn [[_ [task _]]]
+                                         (nil? task)))
+                               (map first)
+                               )
+        assignments (map vector available-workers available-tasks)
+        ]
+    (reduce (fn [state' [worker task]]
+              (-> state'
+                  (assoc-in [:workers worker] [task (task-time task)])
+                  (update :remaining #(dissoc % task))
+                  ))
+            state
+            assignments
+            )))
+
+(defn perform-work [state]
+  (->> (keys (:workers state))
+       (reduce (fn [state' worker]
+                 (update-in state' [:workers worker 1] dec))
+               state
+               )
+       ))
+
+(defn finish-work [state]
+  (->> state
+       (:workers)
+       (filter (fn [[worker [task time-left]]]
+                 (= 0 time-left)
+                 ))
+       (reduce (fn [state' [worker [task _]]]
+                 (-> state'
+                     (update :completed #(conj % task))
+                     (assoc-in [:workers worker] [nil 0])
+                     (update :remaining (fn [remaining]
+                                          (->> remaining
+                                               (map (fn [[t prereqs]]
+                                                      [t (disj prereqs task)]))
+                                               (into {})
+                                               )))
+                     ))
+               state
+               )
+       )
+  )
+
+(defn timestep [state]
+    (-> state
+         (assign-work)
+         (perform-work)
+         (finish-work)
+         (update :time inc)
+         ))
+
+(defn finished? [state]
+  (and (empty? (:remaining state))
+       (every? (fn [[worker [task time-left]]] (nil? task)) (:workers state))
+       ))
 
 (defn solve-a [lines]
   (->> lines
@@ -64,7 +152,13 @@ Step F must be finished before step E can begin.")
        ))
 
 (defn solve-b [lines]
-  (
+  (->> lines
+       (parse-dependencies)
+       (start-timed)
+       (iterate timestep)
+       (filter finished?)
+       (first)
+       (:time)
        ))
 
 (defn run [input-lines & args]
