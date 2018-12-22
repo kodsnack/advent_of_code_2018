b586b0047ce0d96b21997c895b323a86e6a2c09d
Emil Lundberg <emil@emlun.se>
2018-12-22 23:01:47 +0100

    Solve day 9a

--- src/adventofcode/2018/day09.clj
+++ src/adventofcode/2018/day09.clj
@@ -9,8 +9,8 @@
 
 (defn start [state]
   (assoc state
-         :circle (list 0)
-         :current-marble 0
+         :front [0]
+         :back ()
          :next-marble 1
          :player 0
          :scores (->> state
@@ -21,33 +21,53 @@
                       )
          ))
 
+(defn shift-circle-forward [{:keys [front back] :as state} steps]
+  (let [[backfront backback] (split-at steps back)]
+    (if (= steps (count backfront))
+      (-> state
+          (update :front #(apply conj % backfront))
+          (assoc :back backback)
+          )
+      (-> state
+          (assoc :front [])
+          (assoc :back (concat front back))
+          (recur (- steps (count backfront)))
+          ))))
+
+(defn shift-circle-backward [{:keys [front back] :as state} steps]
+  (let [front-split-i (- (count front) steps)]
+    (if (> front-split-i 0)
+      (let [frontfront (subvec front 0 front-split-i)
+            frontback (subvec front front-split-i)
+            ]
+        (assoc state
+               :front frontfront
+               :back (concat frontback back)
+               ))
+      (-> state
+          (update :front #(apply conj % back))
+          (assoc :back ())
+          (recur (- front-split-i))
+          ))))
+
 (defn place [state]
-  (let [place-index (-> state
-                        :current-marble
-                        (+ 2)
-                        (mod (count (:circle state))))
-        place-index (if (= 0 place-index) (count (:circle state)) place-index)
-        ]
-    (-> state
-        (update :circle (fn [circle]
-                          (let [[front back] (split-at place-index circle)]
-                            (concat front [(:next-marble state)] back)
-                            )))
-        (assoc :current-marble place-index)
-        )))
+  (-> state
+      (shift-circle-forward 1)
+      (update :front #(conj % (:next-marble state)))
+      ))
 
 (defn score [state]
-  (let [point-index (-> state
-                        (:current-marble)
-                        (- 7)
-                        (mod (count (:circle state))))
-        [front [point & rest]] (split-at point-index (:circle state))
-        ]
-    (-> state
-        (update-in [:scores (:player state)] #(+ % point (:next-marble state)))
-        (assoc :circle (concat front rest))
-        (assoc :current-marble point-index)
-        )))
+  (-> state
+      (shift-circle-backward 7)
+      (as-> $ (update-in $ [:scores (:player $)]
+                         (fn [score]
+                           (+ score
+                              (get-in $ [:front (dec (count (:front $)))])
+                              (:next-marble $)
+                              ))))
+      (update :front #(subvec % 0 (dec (count %))))
+      (shift-circle-forward 1)
+      ))
 
 (defn step [state]
   (let [score-turn (= 0 (mod (:next-marble state) 23))]
@@ -64,7 +84,6 @@
        (start)
        (iterate step)
        (filter (fn [state]
-                 (println (:next-marble state))
                  (> (:next-marble state) (:last-marble state))))
        (first)
        (:scores)
