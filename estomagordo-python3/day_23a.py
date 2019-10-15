0b0a38db7992d09907fee47f80d1682050490c3b
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-10-15 11:58:33 +0200

    simplified 23a

--- estomagordo-python3/day_23a.py
+++ estomagordo-python3/day_23a.py
@@ -2,29 +2,22 @@ import re
 
 
 def distance(a, b):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1]) + abs(a[2] - b[2])
+    return sum(abs(a[x] - b[x]) for x in range(3))
 
 
 def solve(bots):
-    radiest = 0
+    radiest = max(bot[3] for bot in bots)
+    
+    inrange = 0
 
-    for i, bot in enumerate(bots):
-        if bot[3] > radiest:
-            radiest = bot[3]
-
-    inrange = []
-
-    for i, bot in enumerate(bots):
-        c = 0
-        if bot[3] != radiest:
+    for bota in bots:        
+        if bota[3] != radiest:
             continue
-        for j, bott in enumerate(bots):
-            if distance(bot, bott) <= bot[3]:
-                c += 1
-        inrange.append(c)
+        for botb in bots:
+            if distance(bota, botb) <= bota[3]:
+                inrange += 1
     
-    return inrange[0]
-    
+    return inrange
 
 def read_and_solve():
     pattern = re.compile('-?\d+')
