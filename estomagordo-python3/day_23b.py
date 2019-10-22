e7d7b136c263e9602105c6d323342facf67cbec3
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2019-10-22 17:29:49 +0200

    a *ing this, might wanna check out tiebreaker tho

--- estomagordo-python3/day_23b.py
+++ estomagordo-python3/day_23b.py
@@ -1,24 +1,24 @@
 import re
-
+from heapq import heappop, heappush
 
 def reaches_box(xmin, xmax, ymin, ymax, zmin, zmax, bot):
     x, y, z, r = bot
 
-    ans = xmin <= x <= xmax and ymin <= y <= ymax and zmin <= z <= zmax
+    ans = xmin <= x < xmax and ymin <= y < ymax and zmin <= z < zmax
 
     if not ans:
         if x < xmin:
             r -= (xmin - x)
-        elif x > xmax:
-            r -= (x - xmax)
+        elif x >= xmax:
+            r -= (x - xmax + 1)
         if y < ymin:
             r -= (ymin - y)
-        elif y > ymax:
-            r -= (y - ymax)
+        elif y >= ymax:
+            r -= (y - ymax + 1)
         if z < zmin:
             r -= (zmin - z)
-        elif z > zmax:
-            r -= (z - zmax)
+        elif z >= zmax:
+            r -= (z - zmax + 1)
 
         ans = r >= 0
         
@@ -33,61 +33,29 @@ def solve(bots):
     zmin = min(bot[2] for bot in bots)
     zmax = max(bot[2] for bot in bots)
     
-    lowest_diff = min(xmax - xmin, ymax - ymin, zmax - zmin)
-    
-    step = 1
-    
-    while step < lowest_diff:
-        step *= 2
-        
-    step //= 4
-    from collections import defaultdict
-    d = defaultdict(int)
-    
-    def find(xmin, xmax, ymin, ymax, zmin, zmax, step, d, highest):
-        d[step] += 1
-        if step == 1:
-            best = (0, 10**10)
-            for x in range(xmin, xmax + 1, step):
-                for y in range(ymin, ymax + 1, step):
-                    for z in range(zmin, zmax + 1, step):
-                        score = sum(reaches_box(x, x, y, y, z, z, bot) for bot in bots)
-                        dist = abs(x) + abs(y) + abs(z)
-                        if score > best[0] or (score == best[0] and dist < best[1]):
-                            best = (score, dist)
+    frontier = [(-len(bots), xmin, xmax, ymin, ymax, zmin, zmax)]
 
-            highest['highest'] = max(highest['highest'], best[0])
-            return best
+    while frontier:
+        negpotential, xmin, xmax, ymin, ymax, zmin, zmax = heappop(frontier)
 
-        candidates = []
-        best = 0
+        if xmin == xmax - 1 and ymin == ymax  - 1 and zmin == zmax - 1:
+            return xmin + ymin + zmin
 
-        for x in range(xmin, xmax + 1, step):
-            for y in range(ymin, ymax + 1, step):
-                for z in range(zmin, zmax + 1, step):
-                    score = sum(reaches_box(x, x + step, y, y + step, z, z + step, bot) for bot in bots)
-                    if score > highest['highest']:
-                        if score > best:
-                            candidates = [(x, x + step, y, y + step, z, z + step)]
-                            best = score
-                        elif score == best:
-                            candidates.append((x, x + step, y, y + step, z, z + step))
+        xhalf = (xmin + xmax) // 2
+        yhalf = (ymin + ymax) // 2
+        zhalf = (zmin + zmax) // 2
 
-        values = [find(candidate[0], candidate[1], candidate[2], candidate[3], candidate[4], candidate[5], step // 2, d, highest) for candidate in candidates]
-        values = [v for v in values if v]
-        
-        if values:
-            best = values[0]
+        def do(xmin, xmax, ymin, ymax, zmin, zmax):
+            return (-sum(reaches_box(xmin, xmax, ymin, ymax, zmin, zmax, bot) for bot in bots), xmin, xmax, ymin, ymax, zmin, zmax)
 
-            for value in values[1:]:
-                if value[0] > best[0] or (value[0] == best[0] and value[1] < best[1]):
-                    best = value
-
-            return best
-
-    _, distance = find(xmin, xmax, ymin, ymax, zmin, zmax, step, d, {'highest': 0})
-
-    return distance
+        heappush(frontier, do(xmin, xhalf, ymin, yhalf, zmin, zhalf))
+        heappush(frontier, do(xmin, xhalf, ymin, yhalf, zhalf, zmax))
+        heappush(frontier, do(xmin, xhalf, yhalf, ymax, zmin, zhalf))
+        heappush(frontier, do(xmin, xhalf, yhalf, ymax, zhalf, zmax))
+        heappush(frontier, do(xhalf, xmax, ymin, yhalf, zmin, zhalf))
+        heappush(frontier, do(xhalf, xmax, ymin, yhalf, zhalf, zmax))
+        heappush(frontier, do(xhalf, xmax, yhalf, ymax, zmin, zhalf))
+        heappush(frontier, do(xhalf, xmax, yhalf, ymax, zhalf, zmax))
     
 def read_and_solve():
     pattern = re.compile('-?\d+')
